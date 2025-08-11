4626e2a87d578281eaac3c0062c945dab8b81a95
taylor <taylor.king206@u.boisestate.edu>
2025-08-11 11:11:04 -0600

    make unit test

---
+++ src/utils/cleanup.ts
@@ -0,0 +1,11 @@
+
+const listenrs = new Set<() => void>();
+
+export const onExit = (callback: () => void) => {
+    listenrs.add(callback);
+}
+
+process.on('exit', () => {
+    // cleanup logic if needed
+    listenrs.forEach((cb) => cb());
+});
